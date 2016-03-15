7a2094b6f07b0ef00c6849f569fa804235c0ebef
Nitish Malhotra <nitish.malhotra@gmail.com>
2016-03-15 18:22:40 -0400

    Form Validation exercise

--- AngularJS/conFusion/app/scripts/app.js
+++ AngularJS/conFusion/app/scripts/app.js
@@ -5,6 +5,7 @@ angular.module('confusionApp',[])
 
     $scope.tab = 1;
     $scope.filtText = '';
+    $scope.showDetails = false;
 
     $scope.dishes=[
         {
@@ -64,4 +65,47 @@ angular.module('confusionApp',[])
     $scope.isSelected = function (checkTab) {
         return ($scope.tab === checkTab);
     };
-}]);
+
+    $scope.toggleDetails = function() {
+        $scope.showDetails = !$scope.showDetails;
+    };
+}])
+.controller('ContactController', ['$scope', function ($scope) {
+    $scope.feedback = {
+        mychannel:"",
+        firstName:"",
+        lastName:"",
+        agree:false,
+        email:""
+    };
+
+    var channels = [{value:'tel', label:'Tel.'},
+                    {value:'Email', label:'Email'}];
+
+    $scope.channels = channels;
+    $scope.invalidChannelSelection = false;
+}])
+.controller('FeedbackController', ['$scope', function ($scope) {
+    $scope.sendFeedback = function() {
+        console.log($scope.feedback);
+
+        if($scope.feedback.agree && ($scope.feedback.mychannel == "")) {
+            $scope.invalidChannelSelection = true;
+            console.log('incorrect');
+        }
+        else {
+            $scope.invalidChannelSelection = false;
+            $scope.feedback = {
+                "mychannel":"",
+                "firstName":"",
+                lastName:"",
+                agree:false,
+                email:""
+            };
+            $scope.feedback.mychannel = "";
+            $scope.feedbackForm.$setPristine();
+            console.log($scope.feedback);
+        }
+    };
+}])
+;
