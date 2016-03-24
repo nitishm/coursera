efcd20c4f24b6e1e4a30cb2283fcd49853d8df4d
Nitish Malhotra <nitish.malhotra@gmail.com>
2016-03-23 22:33:12 -0400

    Http error handling

--- AngularJS/conFusion/app/scripts/controllers.js
+++ AngularJS/conFusion/app/scripts/controllers.js
@@ -7,15 +7,18 @@ angular.module('confusionApp')
             $scope.tab = 1;
             $scope.filtText = '';
             $scope.showDetails = false;
-
+            $scope.showMenu = false;
             $scope.dishes = {};
+            $scope.message = "Loading ...";
 
             menuFactory.getDishes()
             .then(
                 function(response) {
-
                     $scope.dishes = response.data;
-
+                    $scope.showMenu = true;
+                },
+                function(response) {
+                    $scope.message = "Error: "+ response.status + " " + response.statusText;
                 }
             );
 
@@ -79,13 +82,17 @@ angular.module('confusionApp')
 
         .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
             $scope.dish = {};
+            $scope.showDish = false;
+            $scope.message = "Loading ...";
 
             menuFactory.getDish(parseInt($stateParams.id,10))
             .then(
                 function(response) {
-
                     $scope.dish = response.data;
-
+                    $scope.showDish = true;
+                },
+                function(response) {
+                    $scope.message = "Error: " + response.status + " " + response.statusText;
                 }
             );
 
@@ -112,12 +119,17 @@ angular.module('confusionApp')
         .controller('IndexController', ['$scope', 'menuFactory', 'corporateFactory', function ($scope, menuFactory, corporateFactory) {
 
             $scope.featuredDish = {};
+            $scope.showDish = false;
+            $scope.message = "Loading ...";
+
             menuFactory.getDish(0)
             .then(
                 function(response) {
-
                     $scope.featuredDish = response.data;
-
+                    $scope.showDish = true;
+                },
+                function(response) {
+                    $scope.message = "Error: " + response.status + " " + response.statusText;
                 }
             );
             $scope.promotion = menuFactory.getPromotion(0);
